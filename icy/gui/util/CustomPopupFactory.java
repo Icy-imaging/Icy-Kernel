a6c2da56243e5481d0ceb7fdb9d0ecacf9c42400
Stephane <stephane.dallongeville@pasteur.fr>
2012-09-14 19:07:37 +0200

    Fix CustomPopupFactory for OSX.

--- icy/gui/util/CustomPopupFactory.java
+++ icy/gui/util/CustomPopupFactory.java
@@ -19,79 +19,96 @@
 package icy.gui.util;
 
 import icy.system.SystemUtil;
+import icy.util.ReflectionUtil;
 
 import java.awt.Component;
-import java.awt.Window;
+import java.lang.reflect.Method;
 
-import javax.swing.JRootPane;
 import javax.swing.Popup;
 import javax.swing.PopupFactory;
-import javax.swing.RootPaneContainer;
 
-/**
- * @author stephane
- */
 public class CustomPopupFactory extends PopupFactory
 {
-    private static final Float OPAQUE = new Float(1.0F);
+//    private static final Float OPAQUE = new Float(1.0F);
 
     private final boolean macos;
 
-    /**
-     * this is to mimic osx style
-     */
-    boolean windowShadowEnabled;
+    private Method getPopupMethod;
+    private int heavy;
 
     public CustomPopupFactory()
     {
         super();
 
+        getPopupMethod = null;
+        heavy = 0;
+
         macos = SystemUtil.isMac();
-        // this is specific to apple os
-        windowShadowEnabled = macos;
-    }
 
-    public boolean isWindowShadowEnabled()
-    {
-        return windowShadowEnabled;
-    }
-
-    public void setWindowShadowEnabled(boolean windowShadowEnabled)
-    {
-        // only for mac os
-        this.windowShadowEnabled = windowShadowEnabled & macos;
-    }
-
-    private static Window getWindow(Component component)
-    {
-        Object obj;
-        for (obj = component; !(obj instanceof Window) && obj != null; obj = ((Component) (obj)).getParent())
-            ;
-        return (Window) obj;
-    }
-
-    @Override
-    public Popup getPopup(Component component, Component component1, int x, int y)
-    {
         if (macos)
         {
-            // this is intended to force Heavy Weight popup component
-            final Popup popup = super.getPopup(null, component1, x, y);
+            try
+            {
+                getPopupMethod = ReflectionUtil.getMethod(PopupFactory.class, "getPopup", true, Component.class,
+                        Component.class, int.class, int.class, int.class);
+                heavy = ReflectionUtil.getField(PopupFactory.class, "HEAVY_WEIGHT_POPUP", true).getInt(null);
+            }
+            catch (Exception e)
+            {
+                getPopupMethod = null;
+                heavy = 0;
+            }
+        }
+    }
 
-            final Window window = getWindow(component1);
+    // private static Window getWindow(Component component)
+    // {
+    // Object obj;
+    // for (obj = component; !(obj instanceof Window) && obj != null; obj = ((Component)
+    // (obj)).getParent())
+    // ;
+    // return (Window) obj;
+    // }
 
-            if (window == null)
-                return popup;
-            if (!(window instanceof RootPaneContainer))
-                return popup;
-
-            final JRootPane popupRootPane = ((RootPaneContainer) window).getRootPane();
-            popupRootPane.putClientProperty("Window.alpha", OPAQUE);
-            popupRootPane.putClientProperty("Window.shadow", Boolean.FALSE);
-
-            return popup;
+    @Override
+    public Popup getPopup(Component owner, Component contents, int x, int y)
+    {
+        if (contents == null)
+        {
+            throw new IllegalArgumentException("Popup.getPopup must be passed non-null contents");
         }
 
-        return super.getPopup(component, component1, x, y);
+        if (macos && (getPopupMethod != null))
+        {
+            try
+            {
+                return (Popup) getPopupMethod.invoke(this, owner, contents, Integer.valueOf(x), Integer.valueOf(y),
+                        Integer.valueOf(heavy));
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+
+            // popup = getPopup(owner, contents, x, y, HEAVY_WEIGHT_POPUP);
+            //
+            // // this is intended to force Heavy Weight popup component
+            // final Popup popup = super.getPopup(null, component1, x, y);
+            //
+            // final Window window = getWindow(component1);
+            //
+            // if (window == null)
+            // return popup;
+            // if (!(window instanceof RootPaneContainer))
+            // return popup;
+            //
+            // final JRootPane popupRootPane = ((RootPaneContainer) window).getRootPane();
+            // popupRootPane.putClientProperty("Window.alpha", OPAQUE);
+            // popupRootPane.putClientProperty("Window.shadow", Boolean.FALSE);
+            //
+            // return popup;
+        }
+
+        return super.getPopup(owner, contents, x, y);
     }
 }
