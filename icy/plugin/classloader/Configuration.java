cd3cfe5f7f42c716b1f56a5d51b315e7151dd00d
Stephane <stephane.dallongeville@pasteur.fr>
2013-04-17 16:59:31 +0200

    updated header

--- icy/plugin/classloader/Configuration.java
+++ icy/plugin/classloader/Configuration.java
@@ -1,27 +1,20 @@
-/**
- *  JCL (Jar Class Loader)
- *
- *  Copyright (C) 2011  Kamran Zafar
- *
- *  This file is part of Jar Class Loader (JCL).
- *  Jar Class Loader (JCL) is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  JarClassLoader is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with JCL.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  @author Kamran Zafar
- *
- *  Contact Info:
- *  Email:  xeus.man@gmail.com
- *  Web:    http://xeustech.blogspot.com
+/*
+ * Copyright 2010-2013 Institut Pasteur.
+ * 
+ * This file is part of Icy.
+ * 
+ * Icy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Icy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Icy. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package icy.plugin.classloader;
--- icy/system/IcyExceptionHandler.java
+++ icy/system/IcyExceptionHandler.java
@@ -1,20 +1,20 @@
 /*
- * Copyright 2010, 2011 Institut Pasteur.
+ * Copyright 2010-2013 Institut Pasteur.
  * 
- * This file is part of ICY.
+ * This file is part of Icy.
  * 
- * ICY is free software: you can redistribute it and/or modify
+ * Icy is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * ICY is distributed in the hope that it will be useful,
+ * Icy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with ICY. If not, see <http://www.gnu.org/licenses/>.
+ * along with Icy. If not, see <http://www.gnu.org/licenses/>.
  */
 package icy.system;
 
