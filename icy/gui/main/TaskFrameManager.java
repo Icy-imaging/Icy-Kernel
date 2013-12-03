93e90501e4ab49992de0a6d976f6d478906cc52b
Timothée Lecomte <timothee.lecomte@pasteur.fr>
2013-12-03 10:08:07 +0100

    (TaskFrameManager) Move the animation code in a separate function
    No code change at all. Just to make it easier to review the next changes.

--- icy/gui/main/TaskFrameManager.java
+++ icy/gui/main/TaskFrameManager.java
@@ -138,158 +138,163 @@ public class TaskFrameManager implements Runnable
     {
         while (true)
         {
-            long currentMillisecondTime = System.currentTimeMillis();
-            long delayBetween2Animation = currentMillisecondTime - lastAnimationMillisecondTime;
-            lastAnimationMillisecondTime = currentMillisecondTime;
-
-            HashMap<TaskFrame, FrameInformation> frameInfos;
-
-            synchronized (taskFrameInfos)
-            {
-                frameInfos = new HashMap<TaskFrame, FrameInformation>(taskFrameInfos);
-            }
-
-            // process frame which become visible and the one which will be closed
-            for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
-            {
-                final TaskFrame frame = finfo.getKey();
-                final FrameInformation info = finfo.getValue();
-
-                info.msBeforeDisplay -= delayBetween2Animation;
-
-                if (info.isVisible())
-                {
-                    if (!frame.isVisible())
-                        frame.setVisible(true);
-                }
-
-                if (frame.canRemove())
-                    info.setMsBeforeClose(info.msBeforeClose - delayBetween2Animation);
-            }
-
-            // get bottom right border location
-            final Dimension desktopSize = getDesktopSize();
-            // not yet initialized
-            if (desktopSize == null)
-                return;
-
-            final List<TaskFrame> toRemove = new ArrayList<TaskFrame>();
-
-            for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
-            {
-                final TaskFrame frame = finfo.getKey();
-                final FrameInformation info = finfo.getValue();
-
-                // frame need to be removed ?
-                if ((info != null) && info.canBeRemoved)
-                {
-                    // get frame position
-                    final Point location = frame.getLocation();
-
-                    // frame already hidden ?
-                    if ((!frame.isVisible()) || ((location.x > desktopSize.width)))
-                    {
-                        // remove it from list
-                        toRemove.add(frame);
-                        // and close it definitely
-                        frame.internalClose();
-                    }
-                }
-            }
-
-            // remove frame which are complete from the list
-            for (TaskFrame frame : toRemove)
-                frameInfos.remove(frame);
-
-            // update global list
-            taskFrameInfos = new HashMap<TaskFrame, FrameInformation>(frameInfos);
-
-            // calculate current Y position
-            float currentY = desktopSize.height;
-            for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
-            {
-                final FrameInformation info = finfo.getValue();
-
-                if ((info != null) && info.isVisible())
-                    currentY -= finfo.getKey().getHeight();
-            }
-
-            // calculate all frames position
-            for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
-            {
-                final TaskFrame frame = finfo.getKey();
-                final FrameInformation info = finfo.getValue();
-
-                if ((info != null) && info.isVisible())
-                {
-                    int xTarget = desktopSize.width - frame.getWidth();
-
-                    if (info.canBeRemoved)
-                        xTarget = desktopSize.width + 20;
-
-                    final Point positionTarget = new Point(xTarget, (int) currentY);
-                    final Point positionCurrent = frame.getLocation();
-
-                    float vectX = (positionTarget.x - positionCurrent.x) / 10f;
-                    if (vectX != 0f)
-                    {
-                        // we want at least 1 or -1
-                        if (Math.abs(vectX) < 1f)
-                        {
-                            if (vectX < 0)
-                                vectX = -1f;
-                            else
-                                vectX = 1f;
-                        }
-                    }
-                    float vectY = (positionTarget.y - positionCurrent.y) / 10f;
-                    if (vectY != 0f)
-                    {
-                        // we want at least 1 or -1
-                        if (Math.abs(vectY) < 1f)
-                        {
-                            if (vectY < 0)
-                                vectY = -1f;
-                            else
-                                vectY = 1f;
-                        }
-                    }
-                    final Point positionNew = new Point((int) (positionCurrent.x + vectX),
-                            (int) (positionCurrent.y + vectY));
-
-                    // set Y when starting the scroll.
-                    if (positionCurrent.x > desktopSize.width)
-                        positionNew.y = positionTarget.y;
-
-                    // update position
-                    info.position = positionNew;
-
-                    // no enough space to display frame, close it
-                    if (positionNew.y < 0)
-                        frame.close();
-
-                    currentY += frame.getHeight();
-                }
-            }
-
-            // update frame position
-            for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
-            {
-                final TaskFrame frame = finfo.getKey();
-                final FrameInformation info = finfo.getValue();
-
-                if (info.position != null)
-                {
-                    // avoid repaint on JDesktopPane if position did not changed
-                    if (!frame.getLocationInternal().equals(info.position))
-                        frame.setLocationInternal(info.position);
-                    if (!frame.getLocationExternal().equals(info.position))
-                        frame.setLocationExternal(info.position);
-                }
-            }
+        	animateFrames();
 
             // sleep a bit
             ThreadUtil.sleep(20);
+        }
+    }
+    
+    void animateFrames()
+    {
+        long currentMillisecondTime = System.currentTimeMillis();
+        long delayBetween2Animation = currentMillisecondTime - lastAnimationMillisecondTime;
+        lastAnimationMillisecondTime = currentMillisecondTime;
+
+        HashMap<TaskFrame, FrameInformation> frameInfos;
+
+        synchronized (taskFrameInfos)
+        {
+            frameInfos = new HashMap<TaskFrame, FrameInformation>(taskFrameInfos);
+        }
+
+        // process frame which become visible and the one which will be closed
+        for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
+        {
+            final TaskFrame frame = finfo.getKey();
+            final FrameInformation info = finfo.getValue();
+
+            info.msBeforeDisplay -= delayBetween2Animation;
+
+            if (info.isVisible())
+            {
+                if (!frame.isVisible())
+                    frame.setVisible(true);
+            }
+
+            if (frame.canRemove())
+                info.setMsBeforeClose(info.msBeforeClose - delayBetween2Animation);
+        }
+
+        // get bottom right border location
+        final Dimension desktopSize = getDesktopSize();
+        // not yet initialized
+        if (desktopSize == null)
+            return;
+
+        final List<TaskFrame> toRemove = new ArrayList<TaskFrame>();
+
+        for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
+        {
+            final TaskFrame frame = finfo.getKey();
+            final FrameInformation info = finfo.getValue();
+
+            // frame need to be removed ?
+            if ((info != null) && info.canBeRemoved)
+            {
+                // get frame position
+                final Point location = frame.getLocation();
+
+                // frame already hidden ?
+                if ((!frame.isVisible()) || ((location.x > desktopSize.width)))
+                {
+                    // remove it from list
+                    toRemove.add(frame);
+                    // and close it definitely
+                    frame.internalClose();
+                }
+            }
+        }
+
+        // remove frame which are complete from the list
+        for (TaskFrame frame : toRemove)
+            frameInfos.remove(frame);
+
+        // update global list
+        taskFrameInfos = new HashMap<TaskFrame, FrameInformation>(frameInfos);
+
+        // calculate current Y position
+        float currentY = desktopSize.height;
+        for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
+        {
+            final FrameInformation info = finfo.getValue();
+
+            if ((info != null) && info.isVisible())
+                currentY -= finfo.getKey().getHeight();
+        }
+
+        // calculate all frames position
+        for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
+        {
+            final TaskFrame frame = finfo.getKey();
+            final FrameInformation info = finfo.getValue();
+
+            if ((info != null) && info.isVisible())
+            {
+                int xTarget = desktopSize.width - frame.getWidth();
+
+                if (info.canBeRemoved)
+                    xTarget = desktopSize.width + 20;
+
+                final Point positionTarget = new Point(xTarget, (int) currentY);
+                final Point positionCurrent = frame.getLocation();
+
+                float vectX = (positionTarget.x - positionCurrent.x) / 10f;
+                if (vectX != 0f)
+                {
+                    // we want at least 1 or -1
+                    if (Math.abs(vectX) < 1f)
+                    {
+                        if (vectX < 0)
+                            vectX = -1f;
+                        else
+                            vectX = 1f;
+                    }
+                }
+                float vectY = (positionTarget.y - positionCurrent.y) / 10f;
+                if (vectY != 0f)
+                {
+                    // we want at least 1 or -1
+                    if (Math.abs(vectY) < 1f)
+                    {
+                        if (vectY < 0)
+                            vectY = -1f;
+                        else
+                            vectY = 1f;
+                    }
+                }
+                final Point positionNew = new Point((int) (positionCurrent.x + vectX),
+                        (int) (positionCurrent.y + vectY));
+
+                // set Y when starting the scroll.
+                if (positionCurrent.x > desktopSize.width)
+                    positionNew.y = positionTarget.y;
+
+                // update position
+                info.position = positionNew;
+
+                // no enough space to display frame, close it
+                if (positionNew.y < 0)
+                    frame.close();
+
+                currentY += frame.getHeight();
+            }
+        }
+
+        // update frame position
+        for (Entry<TaskFrame, FrameInformation> finfo : frameInfos.entrySet())
+        {
+            final TaskFrame frame = finfo.getKey();
+            final FrameInformation info = finfo.getValue();
+
+            if (info.position != null)
+            {
+                // avoid repaint on JDesktopPane if position did not changed
+                if (!frame.getLocationInternal().equals(info.position))
+                    frame.setLocationInternal(info.position);
+                if (!frame.getLocationExternal().equals(info.position))
+                    frame.setLocationExternal(info.position);
+            }
         }
     }
 }
