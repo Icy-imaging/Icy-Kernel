fe71c99b352e4496c83277d82cb067a1970a079b
Stephane-D <stephane.dallongeville@pasteur.fr>
2017-06-30 15:16:24 +0200

    version

--- icy/main/Icy.java
+++ icy/main/Icy.java
@@ -105,7 +105,7 @@ public class Icy
     /**
      * Icy Version
      */
-    public static Version version = new Version("1.9.2.2");
+    public static Version version = new Version("1.9.3.0");
 
     /**
      * Main interface
