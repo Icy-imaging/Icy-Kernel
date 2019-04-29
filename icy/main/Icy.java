4058628a66f83f8fed513e682bcc7ae516112aee
Stephane Dallongeville <stephane@outlook.com>
2019-04-29 15:41:56 +0200

    updated version (to tell the difference with 2.0.0.0)

--- icy/main/Icy.java
+++ icy/main/Icy.java
@@ -106,7 +106,7 @@ public class Icy
     /**
      * Icy Version
      */
-    public static Version version = new Version("2.0.0.0");
+    public static Version version = new Version("2.0.1.0");
 
     /**
      * Main interface
